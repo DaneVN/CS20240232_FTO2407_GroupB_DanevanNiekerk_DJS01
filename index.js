1329fe28cef5fd91af04bac71dd066a860255153
DaneVN <Dane.vN23@gmail.com>
2024-10-23 06:44:24 +0200

    Calculation errors FIXED + Variables renamed

--- index.js
+++ index.js
@@ -9,18 +9,20 @@
 let  spacecraft = {
   velocity : 10000, // velocity (km/h)
   acceleration : 3, // acceleration (m/s^2)
-  calcDuration : 3600, // seconds (1 hour)
+  durationInSeconds : 3600, // seconds (1 hour)
   initDistance : 0, // distance (km)
   remainingFuel : 5000, // remaining fuel (kg)
   fuelBurnRate : 0.5, // fuel burn rate (kg/s)
-  // Pick up an error with how the function below is called and make it robust to such errors
-  calcNewVelocity : () => spacecraft.velocity + (spacecraft.acceleration*spacecraft.calcDuration)
-  // 'this.' seems to be giving me problems so for now 'spacecraft.' will be used inside the method
+  calcNewVelocity : () => {
+    const newVelocityMS = (spacecraft.acceleration * spacecraft.durationInSeconds)
+    // 'this.' seems to be giving me problems so for now 'spacecraft.' will be used inside the method
+    return spacecraft.velocity + (newVelocityMS * 3.6) // m/s^2 to km/h conversion
+  } 
 }
 
-const distance2 = spacecraft.initDistance + (spacecraft.velocity*spacecraft.calcDuration) //calcultes new distance
 const velocity2 = spacecraft.calcNewVelocity() //calculates new velocity based on acceleration
-spacecraft.remainingFuel = spacecraft.fuelBurnRate*spacecraft.calcDuration //calculates remaining fuel
+const distance2 = spacecraft.initDistance + (spacecraft.velocity * (spacecraft.durationInSeconds/60/60)) //calcultes new distance (km/h)
+spacecraft.remainingFuel -= spacecraft.fuelBurnRate*spacecraft.durationInSeconds //calculates remaining fuel
 
 console.log(`Corrected New velocity: ${velocity2} km/h`);
 console.log(`Corrected New Distance: ${distance2} km`);
